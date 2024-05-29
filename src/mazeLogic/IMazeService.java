b6176ffb866461067764ac1e74caf15b09b237e0
Chaienko Alexandr <153275738+SkyForce06@users.noreply.github.com>
2024-05-29 22:04:06 +0200

    Update

--- src/mazeLogic/IMazeService.java
+++ src/mazeLogic/IMazeService.java
@@ -5,9 +5,11 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.List;
 
+// Interfejs IMazeService definiuje operacje związane z rozwiązywaniem labiryntów i zapisywaniem wyników.
 public interface IMazeService {
+    // Metoda getSolvePoints zwraca listę punktów reprezentujących rozwiązanie labiryntu na podstawie danych zawartych w obiekcie parser.
     public List<Point> getSolvePoints(MazeParser parser);
 
+    // Metoda saveMaze zapisuje labirynt w określonym pliku, uwzględniając podane punkty rozwiązania i obraz.
     public void saveMaze(File file, List<Point> solvePoints, BufferedImage image);
-
 }
